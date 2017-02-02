bd126072cca61fba13f709166348ccc4c8068551
Clinton <clintondsouza1993@gmail.com>
2017-02-02 12:50:36 +0530

    Initial Commit

---
+++ CalcTestForGeorge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalcTestForGeorge
+//
+//  Created by Clinton D'Souza on 02/02/17.
+//  Copyright © 2017 Clinton D'Souza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
